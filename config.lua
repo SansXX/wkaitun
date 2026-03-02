280398946c69908780fb8e115f1d4f49faf5fc2a
SansXX <91815848+SansXX@users.noreply.github.com>
2026-03-02 20:49:21 +0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,116 @@
+return {
+    ["Fishing"] = {
+        ["Auto Perfect"] = true,
+        ["Random Result"] = false,
+        ["Auto Favorite"] = true,
+        ["Auto Unfavorite"] = false,
+        ["Fish Name"] = {
+            "Sacred Guardian Squid",
+            {Name = "Ruby", Variant = "Gemstone"}
+        },
+        ["Auto Accept Trade"] = true,
+        ["Auto Friend Request"] = true,
+    },
+    ["Auto Trade"] = {
+        ["Enabled"] = true,
+        ["Whitelist Username"] = {"zypherish"},
+        ["Category Fish"] = {
+            "Secret"
+        },
+        ["Fish Name"] = {
+            "Sacred Guardian Squid",
+            {Name = "Ruby", Variant = "Gemstone"}
+        },
+        ["Item Name"] = {},
+    },
+    ["Farm Coin Only"] = {
+        ["Enabled"] = false,
+        ["Target"] = 190000,
+    },
+    ["Selling"] = {
+        ["Auto Sell"] = true,
+        ["Auto Sell Threshold"] = "Mythic",
+        ["Auto Sell Every"] = 50,
+    },
+    ["Doing Quest"] = {
+        ["Auto Ghostfinn Rod"] = true,
+        ["Auto Element Rod"] = false,
+        ["Auto Element Rod 2"] = true,
+        ["Auto Diamond Rod"] = false,
+        ["Unlock Ancient Ruin"] = false,
+        ["Allowed Sacrifice"] = {"Ghost Shark", "Cryoshade Glider", "Panther Eel", "Queen Crab", "King Crab", "Giant Squid", "Blob Shark", "Ghost Shark"},
+        ["FARM_LOC_SECRET_SACRIFICE"] = "Ocean",
+        ["Minimum Rod"] = "Astral Rod",
+    },
+    ["WebHook"] = {
+        ["Link Webhook"] = "https://discord.com/api/webhooks/1452659546269945919/vxfmdMu7Xy9Pa7dieymEXL35zbiciXB7V9dNytalZFQ_52LbmlHIQHeM2CDLAN2ielmC",
+        ["Auto Sending"] = true,
+        ["Category"] = {"Secret"},
+        ["Link Webhook Quest Complete"] = "https://discord.com/api/webhooks/1469652278658207898/xH-N5v4es6QAM-pKCx4GZnzuCv7r21kesgNphXf05IhsdIf6UlhEcBrXrHgGdtbkjKh8",
+    },
+    ["Weather"] = {
+        ["Auto Buying"] = true,
+        ["Minimum Rod"] = "Astral Rod",
+        ["Weather List"] = {"Wind", "Storm", "Cloudy"},
+    },
+    ["Potions"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Astral Rod",
+    },
+    ["Totems"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Ghostfinn Rod",
+        ["Buy List"] = {
+            ["Mutation Totem"] = 10
+        },
+    },
+    ["Event"] = {
+        ["Start Farm"] = true,
+        ["Minimum Rod"] = "Ghostfinn Rod",
+        ["Event List"] = {
+            ["Ancient Lochness Monster"] = true,
+            "Megalodon Hunt"
+        },
+    },
+    ["Enchant"] = {
+        ["Auto Enchant"] = true,
+        ["Roll Enchant"] = false,
+        ["Evolved Roll Enchant"] = true,
+        ["Enchant List"] = {"SECRET Hunter"},
+        ["Second Enchant"] = true,
+        ["Allowed Sacrifice"] = {"Cryoshade Glider", "Ghost Shark", "Blob Shark", "Giant Squid", "King Crab", "Queen Crab"},
+        ["Second Enchant List"] = {"Cursed I"},
+        ["Minimum Rod"] = "Ghostfinn Rod",
+    },
+    ["Bait List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {"Midnight Bait", "Chroma Bait", "Corrupt Bait", "Singularity Bait"},
+        ["Endgame"] = "Singularity Bait",
+    },
+    ["Rod List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {
+            "Grass Rod",
+            "Midnight Rod",
+            "Astral Rod",
+            "Ares Rod",
+            "Angler Rod",
+            "Steampunk Rod",
+        },
+        ["Location Rods"] = {
+            ["Fisherman Island"] = {"Starter Rod"},
+            ["Kohana Volcano"] = {"Grass Rod", "Midnight Rod"},
+            ["Sisyphus Statue"] = {"Angler Rod", "Ares Rod", "Astral Rod"},
+            ["Treasure Room"] = {"Element Rod", "Ghostfinn Rod"},
+        },
+        ["Endgame"] = "Diamond Rod",
+    },
+    
+    ["ExtremeFpsBoost"] = true,
+    ["UltimatePerformance"] = true,
+    ["Disable3DRender"] = true,
+    ["AutoRemovePlayer"] = true,
+    ["AutoReconnect"] = false,
+    ["HideGUI"] = false,
+    ["EXIT_MAP_IF_DISCONNECT"] = true,
+}
