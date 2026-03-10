1e26a7ba362c39f6a6b1a20f47fc8708af411b6a
SansXX <91815848+SansXX@users.noreply.github.com>
2026-03-10 21:12:27 +0700

    Update 62iJXylle5Ph6PkG7CdX.lua

--- 62iJXylle5Ph6PkG7CdX.lua
+++ 62iJXylle5Ph6PkG7CdX.lua
@@ -47,7 +47,7 @@ return {
     ["WebHook"] = {
         ["Link Webhook"] = "https://discord.com/api/webhooks/1452659546269945919/vxfmdMu7Xy9Pa7dieymEXL35zbiciXB7V9dNytalZFQ_52LbmlHIQHeM2CDLAN2ielmC",
         ["Auto Sending"] = true,
-        ["Category"] = {"Secret"},
+        ["Category"] = {"Secret"}, {Name = "Ruby", Variant = "Gemstone"},
         ["Link Webhook Quest Complete"] = "https://discord.com/api/webhooks/1469652278658207898/xH-N5v4es6QAM-pKCx4GZnzuCv7r21kesgNphXf05IhsdIf6UlhEcBrXrHgGdtbkjKh8",
     },
     ["Weather"] = {
